d4e980a1c9dbd8b3781932eaf415516ac7ce6715
Atharva Thakur <atharvathakur369@gmail.com>
2024-07-01 01:08:06 +0530

    Added search component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,15 +3,18 @@ import { RecipeListComponent } from '../recipe-list/recipe-list.component';
 import { CommonModule } from '@angular/common';
 import { FilterPipe } from '../pipes/filter.pipe';
 import { FormsModule } from '@angular/forms';
-import { Ingredient, Nutrition, Recipe } from '../models/recipe.model';
 import { RecipeComponent } from '../recipe/recipe.component';
+import { SearchComponent } from '../search/search.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RecipeListComponent, CommonModule, FilterPipe, FormsModule, RecipeComponent],
+  imports: [RecipeListComponent, CommonModule, FilterPipe, FormsModule, RecipeComponent, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent{
     searchText: string = '';
+    setSearchText(value: string) {
+        this.searchText = value;
+    }
 }
